f8bbe2f58b396d82b08db75273b1840bdfa3c3a7
Joshua Sherer <joshua.sherer@gmail.com>
2015-08-08 22:12:52 +0000

    Some CSS and markup updates

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,20 +3,26 @@ import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
 import del from 'del';
-import {stream as wiredep} from 'wiredep';
+import {
+  stream as wiredep
+}
+from 'wiredep';
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
 gulp.task('favicons', () => {
-    return gulp.src('app/index.html')
-        .pipe($.favicons({
-            files: {
-              dest: 'images/favicon',
-              src: 'app/images/stickfist.png'},
-            settings: { background: '#ffffff' }
-        }))
-        .pipe(gulp.dest('./'));
+  return gulp.src('app/index.html')
+    .pipe($.favicons({
+      files: {
+        dest: 'images/favicon',
+        src: 'app/images/stickfist.png'
+      },
+      settings: {
+        background: '#ffffff'
+      }
+    }))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('styles', () => {
@@ -28,16 +34,23 @@ gulp.task('styles', () => {
       precision: 10,
       includePaths: ['.']
     }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['last 1 version']}))
+    .pipe($.autoprefixer({
+      browsers: ['last 1 version']
+    }))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/styles'))
-    .pipe(reload({stream: true}));
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 function lint(files, options) {
   return () => {
     return gulp.src(files)
-      .pipe(reload({stream: true, once: true}))
+      .pipe(reload({
+        stream: true,
+        once: true
+      }))
       .pipe($.eslint(options))
       .pipe($.eslint.format())
       .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
@@ -53,38 +66,47 @@ gulp.task('lint', lint('app/scripts/**/*.js'));
 gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 
 gulp.task('html', ['styles'], () => {
-  const assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
+  const assets = $.useref.assets({
+    searchPath: ['.tmp', 'app', '.']
+  });
 
   return gulp.src('app/*.html')
     .pipe(assets)
     .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.minifyCss({compatibility: '*'})))
+    .pipe($.if('*.css', $.minifyCss({
+      compatibility: '*'
+    })))
     .pipe(assets.restore())
     .pipe($.useref())
-    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe($.if('*.html', $.minifyHtml({
+      conditionals: true,
+      loose: true
+    })))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
     .pipe($.if($.if.isFile, $.cache($.imagemin({
-      progressive: true,
-      interlaced: true,
-      // don't remove IDs from SVGs, they are often used
-      // as hooks for embedding and styling
-      svgoPlugins: [{cleanupIDs: false}]
-    }))
-    .on('error', function (err) {
-      console.log(err);
-      this.end();
-    })))
+        progressive: true,
+        interlaced: true,
+        // don't remove IDs from SVGs, they are often used
+        // as hooks for embedding and styling
+        svgoPlugins: [{
+          cleanupIDs: false
+        }]
+      }))
+      .on('error', function(err) {
+        console.log(err);
+        this.end();
+      })))
     .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('fonts', () => {
   return gulp.src(require('main-bower-files')({
-    filter: '**/*.{eot,svg,ttf,woff,woff2}'
-  }).concat('app/fonts/**/*'))
+      filter: '**/*.{eot,svg,ttf,woff,woff2}'
+    }).concat('app/fonts/**/*'))
     .pipe(gulp.dest('.tmp/fonts'))
     .pipe(gulp.dest('dist/fonts'));
 });
@@ -100,7 +122,7 @@ gulp.task('extras', () => {
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-gulp.task('serve', ['favicons', 'styles', 'fonts'], () => {
+gulp.task('serve', ['styles', 'fonts'], () => {
   browserSync({
     notify: false,
     port: process.env.PORT,
@@ -132,12 +154,13 @@ gulp.task('serve:dist', () => {
     server: {
       baseDir: ['dist'],
       https: true,
-    }, function (err, bs) {
-    // bs.options.url contains the original url, so
-    // replace the port with the correct one:
-    var url = bs.options.urls.local;
-    require('opn')(url);
-    console.log('Started browserSync on ' + url);
+    },
+    function(err, bs) {
+      // bs.options.url contains the original url, so
+      // replace the port with the correct one:
+      var url = bs.options.urls.local;
+      require('opn')(url);
+      console.log('Started browserSync on ' + url);
     }
   });
 });
@@ -176,8 +199,11 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'favicons', 'extras'], () => {
+  return gulp.src('dist/**/*').pipe($.size({
+    title: 'build',
+    gzip: true
+  }));
 });
 
 gulp.task('default', ['clean'], () => {
